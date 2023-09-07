61a85ea7230bc09291b90d48f4afd6373e23b198
Valinella <oliverdillon10@gmail.com>
2023-09-08 04:49:06 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+function openTabs(event, content) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(content).style.display = "flex";
+    event.currentTarget.className += " active";
+}
+document.getElementById("default").click();
